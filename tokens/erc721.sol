6eef43251f6701bdb5e912b59840a0bec9ddcda1
charlieYCH <newstart0111@gmail.com>
2020-07-14 21:38:04 +0800

    fixed bug

--- tokens/erc721.sol
+++ tokens/erc721.sol
@@ -1,10 +1,10 @@
 pragma solidity 0.6.2;
 
 interface ERC721{
-    
+
   event Transfer(address indexed _from,address indexed _to,uint256 indexed _tokenId);
 
-  event Approval(ddress indexed _owner,address indexed _approved,uint256 indexed _tokenId);
+  event Approval(address indexed _owner,address indexed _approved,uint256 indexed _tokenId);
 
   event ApprovalForAll(address indexed _owner,address indexed _operator,bool _approved);
 
