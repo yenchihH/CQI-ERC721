7e27f0a1549fdc54086914061e518c1111a68ae9
charlieYCH <newstart0111@gmail.com>
2020-07-15 10:43:20 +0800

    test

--- simulates/enumerable.sol
+++ simulates/enumerable.sol
@@ -1,19 +1,19 @@
-pragma solidity 0.6.2;
+// pragma solidity 0.6.2;
 
-import "../../tokens/enumerable-token.sol";
-import "../ownership/ownable.sol";
+// import "../../tokens/token-enumerable.sol";
+// import "../ownership/ownable.sol";
 
-contract CQITokenEnumerable isCQITokenEnumerable,Ownable
-{
+// contract CQITokenEnumerable isCQITokenEnumerable,Ownable
+// {
 
-  function mint(address _to,uint256 _tokenId)external onlyOwner
-  {
-    super._mint(_to, _tokenId);
-  }
+//   function mint(address _to,uint256 _tokenId)external onlyOwner
+//   {
+//     super._mint(_to, _tokenId);
+//   }
 
-  function burn(uint256 _tokenId)external onlyOwner
-  {
-    super._burn(_tokenId);
-  }
+//   function burn(uint256 _tokenId)external onlyOwner
+//   {
+//     super._burn(_tokenId);
+//   }
 
-}
+// }
--- simulates/token.sol
+++ simulates/token.sol
@@ -1,13 +1,13 @@
-pragma solidity 0.6.2;
+// pragma solidity 0.6.2;
 
-import "../../tokens/token.sol";
-import "../ownership/ownable.sol";
+// import "../../tokens/token.sol";
+// import "../ownership/ownable.sol";
 
 
-contract CQITokenMock isCQIToken,Ownable
-{
-  function mint(address _to,uint256 _tokenId)external onlyOwner{
-    super._mint(_to, _tokenId);
-  }
+// contract CQITokenMock isCQIToken,Ownable
+// {
+//   function mint(address _to,uint256 _tokenId)external onlyOwner{
+//     super._mint(_to, _tokenId);
+//   }
 
-}
+// }
