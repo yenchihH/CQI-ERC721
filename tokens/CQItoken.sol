9916d9b0c2914ad0365455689bc479d98d13850d
charlieYCH <newstart0111@gmail.com>
2020-07-16 10:14:09 +0800

    update

--- tokens/CQItoken.sol
+++ tokens/CQItoken.sol
@@ -2,7 +2,7 @@ pragma solidity 0.6.2;
 
 import "./erc721.sol";
 import "./erc721-token-receiver.sol";
-import "./erc721-enumerable.sol"
+import "./erc721-enumerable.sol";
 import "../math/SafeMath.sol";
 import "../utils/interface.sol";
 import "../utils/address-utils.sol";
